2082bb7db15a6b3b535adb64a946fcd2f1b30f9f
Matheus-brum <matheus.brum@walfirm.com.br>
2022-06-14 12:33:33 -0300

    creating method get and delete

--- index.js
+++ index.js
@@ -8,6 +8,38 @@ const app =  express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  const { userId } = req.body;
+
+  db.query("SELECT * FROM city_group WHERE user_id = ?", [userId], (err, result) => {
+    return res.json(result);
+  })
+})
+
+app.post('/', (req, res) => {
+  const { groupName, cities, userId } = req.body;
+
+  console.log(JSON.stringify(cities));
+
+  db.query("INSERT INTO city_group (user_id, name, cities) VALUES (?, ?, ?)", [userId, groupName, JSON.stringify(cities)], (err, result) => {
+    if(err){
+      return res.json(err);  
+    }
+    return res.json(result);
+  })
+})
+
+app.delete('/', (req, res) => {
+  const { groupId } = req.body;
+
+  db.query(`DELETE FROM city_group WHERE id = ?`, [groupId], (err, result) => {
+    if(err){
+      return res.json(err);  
+    }
+    return res.json(result);
+  })
+})
+
 app.listen(8888, () => {
   console.log("rodando na porta 8888")
 })
